3d6f8353ae91188dd0658e2dc3aaab7d151520f0
Win Cheng <winzamark12@gmail.com>
2024-05-28 23:33:40 -0700

    Authentication with Clerk Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="h-30 w-30 flex flex-col border border-black"></div>
+      <div className="h-30 w-30 flex flex-col border border-black">
+        <h1>HELLO WORLD</h1>
+      </div>
     </main>
   );
 }
