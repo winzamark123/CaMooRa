2a9286841bb6cfbab2177d0163c0f8601c7e5d59
Win Cheng <winzamark12@gmail.com>
2024-06-30 13:23:37 -0700

    Banner added and using useUser for client sided

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,22 @@
-import { SignedIn, SignedOut, SignInButton } from '@clerk/nextjs';
+'use client';
+import { useState } from 'react';
+import { SignedIn, SignedOut, useUser } from '@clerk/nextjs';
 import { ModeToggle } from '../Theme/mode-toggle';
 import Image from 'next/image';
 import FoMooLogo from '@/public/fo-moo-logo.svg';
 import Link from 'next/link';
-import { auth } from '@clerk/nextjs/server';
 import { DropDownProfile } from './DropDownProfile';
 import { Button } from '../ui/button';
+import SignInPopUp from '../Popups/SignInPopUp';
 
 export default function NavBar() {
-  const { userId } = auth();
+  const [showSignInPopUp, setShowSignInPopUp] = useState(false);
+
+  const toggleSignInPopUp = () => {
+    setShowSignInPopUp(!showSignInPopUp);
+  };
+  const { user } = useUser();
+
   return (
     <main className="h-30 flex w-full justify-between p-8">
       <Link href="/">
@@ -17,15 +25,16 @@ export default function NavBar() {
       <div className="flex items-center justify-between gap-4 border border-white p-4">
         <p>Feedback</p>
         <ModeToggle />
-        <SignedIn>{userId && <DropDownProfile userId={userId} />}</SignedIn>
+        <SignedIn>{user && <DropDownProfile userId={user.id} />}</SignedIn>
         <div className="flex">
           <SignedOut>
-            <SignInButton>
-              <Button className="px-6 py-3">Get Started</Button>
-            </SignInButton>
+            <Button className="px-6 py-3" onClick={toggleSignInPopUp}>
+              Get Started
+            </Button>
           </SignedOut>
         </div>
       </div>
+      {showSignInPopUp && <SignInPopUp />}
     </main>
   );
 }
--- src/components/Popups/SignInPopUp.tsx
+++ src/components/Popups/SignInPopUp.tsx
@@ -1,7 +1,24 @@
+import { SignInButton } from '@clerk/nextjs';
+import { Button } from '../ui/button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+
 export default function SignInPopUp() {
   return (
-    <main>
-      <div>SignInPopUp</div>
+    <main className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
+      <div
+        className="h-92 left-0 top-1/3 z-30 flex w-9/12 flex-col items-center 
+      justify-center gap-4 bg-sky-900 p-12 text-white"
+      >
+        <h4>Welcome to FoMoo!</h4>
+        <p>Please Sign in with your UCDavis Account</p>
+        <SignInButton>
+          <Button className="flex gap-3 px-6 py-3">
+            <FontAwesomeIcon icon={faGoogle} />
+            Sign In with Google
+          </Button>
+        </SignInButton>
+      </div>
     </main>
   );
 }
