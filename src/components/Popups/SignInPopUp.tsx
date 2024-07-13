17c4dc3510ee4228aff6560f3855953325568389
Win Cheng <winzamark12@gmail.com>
2024-07-12 18:33:02 -0700

    Pop up structure done but inverse theme not working

--- src/components/Popups/SignInPopUp.tsx
+++ src/components/Popups/SignInPopUp.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import { SignInButton } from '@clerk/nextjs';
 import { Button } from '../ui/button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { motion } from 'framer-motion';
+import PopupSVG from '@/public/fo-moo-pop-up.svg';
 
 export default function SignInPopUp({ onToggle }: { onToggle: () => void }) {
   return (
@@ -19,18 +21,23 @@ export default function SignInPopUp({ onToggle }: { onToggle: () => void }) {
           damping: 15,
           duration: 0.5,
         }}
-        className="h-92 left-0 top-1/3 z-30 flex w-9/12 flex-col items-center 
-      justify-center gap-4 rounded-2xl bg-sky-900 p-12 text-white"
+        className="left-0 top-1/3 z-30 flex h-80 w-9/12 justify-end 
+      rounded-2xl bg-sky-900 px-16 text-white"
         onClick={(event) => event.stopPropagation()}
       >
-        <h4>Welcome to FoMoo!</h4>
-        <p>Please Sign in with your UCDavis Account</p>
-        <SignInButton>
-          <Button className="flex gap-3 px-6 py-3">
-            <FontAwesomeIcon icon={faGoogle} />
-            Sign In with Google
-          </Button>
-        </SignInButton>
+        <div className="absolute left-10 flex flex-col gap-4 p-20">
+          <h4>Welcome to FoMoo!</h4>
+          <p>Please Sign in with your UCDavis account</p>
+          <SignInButton>
+            <Button className="dark flex w-fit justify-start gap-3 px-8 py-3">
+              <FontAwesomeIcon icon={faGoogle} />
+              <p>Sign In with Google</p>
+            </Button>
+          </SignInButton>
+        </div>
+        <div className="flex items-end">
+          <Image src={PopupSVG} alt="FoMoo Pop Up" />
+        </div>
       </motion.div>
     </main>
   );
