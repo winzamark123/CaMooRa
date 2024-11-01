a3575d9b745fa0327129cc3d1134a081fdd195bc
Win Cheng <winzamark12@gmail.com>
2024-10-31 17:40:59 -0700

    Got rid of extra grids

--- src/components/Masonry/MasonryGrid.tsx
+++ src/components/Masonry/MasonryGrid.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import { ImageProp } from '@/server/routers/Images';
+import { trpc } from '@/lib/trpc/client';
+import { useState } from 'react';
 
 interface MasonryWrapperProps {
   images: ImageProp[];
 }
 // The number of columns change by resizing the window
 export default function MasonryWrapper({ images }: MasonryWrapperProps) {
+  const deleteImage = trpc.images.deleteImage.useMutation();
+
+  const handleDeleteImage = async (imageId: string) => {
+    console.log('Image ID: ', imageId);
+    const res = await deleteImage.mutate({ imageId: imageId });
+    console.log(res);
+    console.log('Deleted Image');
+    // window.location.reload();
+  };
   return (
     <ResponsiveMasonry
       columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1240: 4 }}
