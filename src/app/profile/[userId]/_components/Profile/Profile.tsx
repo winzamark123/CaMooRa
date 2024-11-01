97abdcb26e06b6bf3150f7526b0a9e0b2fed4aed
Win Cheng <winzamark12@gmail.com>
2024-11-01 15:24:16 -0700

    Refactor contacts

--- src/app/profile/[userId]/_components/Profile/Profile.tsx
+++ src/app/profile/[userId]/_components/Profile/Profile.tsx
@@ -1,17 +1,15 @@
 'use client';
-import { useUser, SignedOut } from '@clerk/nextjs';
 import { useState } from 'react';
 import EditProfile from '@/app/profile/[userId]/_components/Profile/Edit/EditProfile';
 import SignInPopUp from '@/components/Popups/SignInPopUp';
 import { trpc } from '@/lib/trpc/client';
 import { usePathname } from 'next/navigation';
-import { Button } from '../../../../../components/ui/button';
 import Bio from './Bio/Bio';
 import Projects from './Projects';
 import type { Contact } from '@/server/routers/Contact/index';
 import type { Profile } from '@/server/routers/Profile/index';
-import IconComponents from './IconComponents';
 import ProfilePic from './ProfilePic/ProfilePic';
+import Contacts from './Contacts/Contacts';
 
 interface IProfilePic {
   id: string;
@@ -26,7 +24,6 @@ export default function Profile() {
   const [isEditing, setIsEditing] = useState(false);
   const pathname = usePathname();
   const clerkId = pathname.split('/').pop() || '';
-  const { isSignedIn, user: currentUser } = useUser();
   const [showSignInPopUp, setShowSignInPopUp] = useState(false);
 
   const {
@@ -50,27 +47,6 @@ export default function Profile() {
   if (profileError || contactError) {
     return <div>Error occurred</div>;
   }
-
-  const userLinks = [
-    contact?.phone && { type: 'phone', data: contact.phone },
-    contact?.discord && { type: 'discord', data: contact.discord },
-    contact?.whatsApp && { type: 'whatsapp', data: contact.whatsApp },
-    contact?.instagramLink && {
-      type: 'instagram',
-      data: { link: contact.instagramLink, title: contact.instagramTitle },
-    },
-    contact?.portfolioLink && {
-      type: 'portfolio',
-      data: { link: contact.portfolioLink, title: contact.portfolioTitle },
-    },
-    contact?.email && { type: 'email', data: contact.email },
-  ].filter(
-    (link) =>
-      link !== null &&
-      typeof link === 'object' &&
-      'type' in link &&
-      'data' in link
-  );
 
   const toggleEditing = () => {
     setIsEditing(!isEditing);
@@ -119,79 +95,11 @@ export default function Profile() {
               clerkId={clerkId}
             />
           </div>
-          <div className="flex flex-grow flex-col">
-            {isSignedIn && contact?.isContactPublic && (
-              <div className="flex pt-5 sm:justify-end sm:pt-0">
-                <div className="flex flex-col space-y-4 xl:space-y-6">
-                  <h2 className="text-sm font-semibold lg:text-lg 2xl:text-xl">
-                    Contact
-                  </h2>
-                  <ul className="grid list-none grid-cols-2 gap-5 break-all sm:grid-cols-1">
-                    {userLinks.map((link, index) => {
-                      if (!link) return null;
-
-                      const IconComponent = IconComponents[link.type];
-
-                      if (IconComponent) {
-                        return (
-                          <li
-                            key={index}
-                            className="flex items-center space-x-3"
-                          >
-                            <div className="flex items-center space-x-3">
-                              {IconComponent}
-                              {/* Checks for Links  */}
-                              {typeof link.data === 'object' &&
-                              (link.type === 'portfolio' ||
-                                link.type === 'instagram') ? (
-                                <a
-                                  className="cursor-pointer text-sm underline hover:text-blue-800"
-                                  aria-label={`Link to ${usersFullName} ${link.type}`}
-                                  href={link.data.link}
-                                >
-                                  {link.data.title
-                                    ? link.data.title
-                                    : link.type[0].toUpperCase() +
-                                      link.type.slice(1)}
-                                </a>
-                              ) : (
-                                // No Links
-                                <span className="text-sm">
-                                  {typeof link.data === 'string'
-                                    ? link.data
-                                    : link.data.link}
-                                </span>
-                              )}
-                            </div>
-                          </li>
-                        );
-                      }
-                    })}
-                  </ul>
-                </div>
-              </div>
-            )}
-            <div className="mt-auto flex items-end justify-end">
-              {currentUser?.id === clerkId && (
-                <Button
-                  className="mt-8 w-full border border-gray-400 bg-profile_button_bg text-xs text-black hover:bg-primary_blue hover:text-white focus:bg-primary_blue  focus:text-white sm:w-20"
-                  aria-label="Edit your Profile"
-                  onClick={toggleEditing}
-                >
-                  Edit
-                </Button>
-              )}
-              <SignedOut>
-                <Button
-                  onClick={toggleSignInPopUp}
-                  className="mt-8 w-full border border-gray-400 bg-profile_button_bg text-xs text-black hover:bg-primary_blue hover:text-white focus:bg-primary_blue  focus:text-white sm:w-20"
-                  aria-label={`Contact ${usersFullName}`}
-                >
-                  Contact
-                </Button>
-              </SignedOut>
-            </div>
-          </div>
+          <Contacts
+            clerkId={clerkId}
+            toggleSignInPopUp={toggleSignInPopUp}
+            toggleEditing={toggleEditing}
+          />
         </div>
       </div>
       <Projects clerkId={clerkId} />
