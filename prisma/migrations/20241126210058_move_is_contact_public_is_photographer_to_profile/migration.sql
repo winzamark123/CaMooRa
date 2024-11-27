36be913053789d62b1d3b92d6b13dc975b870296
Win Cheng <winzamark12@gmail.com>
2024-11-27 14:53:39 -0800

    adjusted the migration file to move the preferences to profile instead of contact

--- prisma/migrations/20241126210058_move_is_contact_public_is_photographer_to_profile/migration.sql
+++ prisma/migrations/20241126210058_move_is_contact_public_is_photographer_to_profile/migration.sql
@@ -5,10 +5,20 @@
   - You are about to drop the column `isPhotographer` on the `Contact` table. All the data in the column will be lost.
 
 */
--- AlterTable
-ALTER TABLE "Contact" DROP COLUMN "isContactPublic",
-DROP COLUMN "isPhotographer";
+-- First add the new columns to Profile
+ALTER TABLE "Profile" 
+ADD COLUMN "isContactPublic" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN "isPhotographer" BOOLEAN NOT NULL DEFAULT false;
 
--- AlterTable
-ALTER TABLE "Profile" ADD COLUMN     "isContactPublic" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "isPhotographer" BOOLEAN NOT NULL DEFAULT false;
+-- Then copy the existing data from Contact to Profile
+UPDATE "Profile" p
+SET 
+    "isContactPublic" = c."isContactPublic",
+    "isPhotographer" = c."isPhotographer"
+FROM "Contact" c
+WHERE p."userId" = c."userId";
+
+-- Finally, remove the columns from Contact
+ALTER TABLE "Contact" 
+DROP COLUMN "isContactPublic",
+DROP COLUMN "isPhotographer";
